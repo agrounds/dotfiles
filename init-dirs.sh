379f29d6818dd919f4e7ee3a3ec6c7d8bdfa674f
Alex Grounds <agrounds90@gmail.com>
2025-04-30 22:38:22 -0500

    Add vim sensible install to init script

--- init-dirs.sh
+++ init-dirs.sh
@@ -3,3 +3,6 @@
 mkdir -p "$HOME/bin"
 mkdir -p "$HOME/.config"
 [ ! -e "$HOME/.config/starship.toml" ] && cp .config/starship.toml "$HOME/.config/starship.toml"
+
+mkdir -p "$HOME/.vim/pack/tpope/start"
+[ ! -e "$HOME/.vim/pack/tpope/start/vim-sensible" ] && git clone git@github.com:tpope/vim-sensible.git "$HOME/.vim/pack/tpope/start/vim-sensible"
